8365624e8b48654ebd1868b5570feff26ea171b2
Christopher Ross <cross25@uis.edu>
2019-07-07 15:44:59 -0700

    Add project files.

---
+++ DjangoTicTacToe/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\chris\Dropbox\01Code\Visual Studio 2017\DjangoTicTacToe\DjangoTicTacToe\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
